20c7124d807b4392c251a943136e26ad3af3a796
Abhilash Ramadasan <amdzines@Abhilashs-iMac.local>
2020-07-07 09:07:33 +0530

    topic 5 practice

---
+++ PractiseTopic5/app/src/main/java/com/amdzines/practise_topic5/MainActivity.java
@@ -0,0 +1,14 @@
+package com.amdzines.practise_topic5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
